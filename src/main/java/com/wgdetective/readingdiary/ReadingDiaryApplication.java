14302a53c7b0662d105f3c7eb8825e67abde2621
Uladzimir Litvinau <uladzimir_litvinau@epam.com>
2022-11-23 12:52:28 +0300

    initial commit

---
+++ src/main/java/com/wgdetective/readingdiary/ReadingDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.wgdetective.readingdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadingDiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wgdetective/readingdiary/ReadingDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wgdetective.readingdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
